bb4a0ac7e803a1fe306a853844c4fd9d29209b1e
pbusenius <14203989+pbusenius@users.noreply.github.com>
2023-06-04 21:25:49 +0200

    refactor

--- tag_3/baby_step_giant_step.py
+++ tag_3/baby_step_giant_step.py
@@ -11,7 +11,7 @@ def compute(n, p, g):
         x1_values[pow(g, i*A, p)] = i
 
     for x2 in range(A):
-        tmp = (n * pow(g, -x2, p)) % p
+        tmp = (n * pow(g, - x2, p)) % p
         if tmp in x1_values:
             x1 = x1_values[tmp]
             result = A * x1 + x2
